8537e83e3bfb9f17cb37c4f8550dacdeed34da65
Satoshi Matsumoto <kaorimatz@gmail.com>
2020-12-24 16:26:14 +0900

    Fix request ID not being propagated

--- lib/garage_client/request/propagate_request_id.rb
+++ lib/garage_client/request/propagate_request_id.rb
@@ -2,8 +2,8 @@ module GarageClient
   module Request
     class PropagateRequestId < Faraday::Middleware
       def call(env)
-        if Thread.current[:request_id] && !env[:request_headers]["HTTP_X_REQUEST_ID"]
-          env[:request_headers]["HTTP_X_REQUEST_ID"] = Thread.current[:request_id]
+        if Thread.current[:request_id] && !env[:request_headers]["X-Request-Id"]
+          env[:request_headers]["X-Request-Id"] = Thread.current[:request_id]
         end
         @app.call(env)
       end
